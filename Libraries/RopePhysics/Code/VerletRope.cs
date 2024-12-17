61b15584c6636b740960d351d669785c7f263882
ducc <spielerc@gmail.com>
2024-12-17 13:45:27 -0700

    Refactor Verlet to be rope instead of arbitrary segments

---
+++ Libraries/RopePhysics/Code/VerletRope.cs
@@ -0,0 +1,128 @@
+﻿namespace Duccsoft;
+
+public class VerletRope
+{
+	public VerletRope( Vector3 startPos, Vector3 endPos, int pointCount, PhysicsWorld physics = null )
+	{
+		StartPosition = startPos;
+		EndPosition = endPos;
+		PointCount = pointCount;
+
+		Physics = physics;
+		Reset();
+	}
+
+	public struct Point
+	{
+		public Point( Vector3 position, Vector3 lastPosition )
+		{
+			Position = position;
+			LastPosition = lastPosition;
+		}
+
+		public Vector3 Position { get; set; }
+		public Vector3 LastPosition { get; set; }
+	}
+
+	public Vector3 Gravity { get; set; } = Vector3.Down * 800f;
+	public Vector3 StartPosition { get; set; } = Vector3.Zero;
+	public Vector3 EndPosition { get; set; } = Vector3.Down * 100f;
+	public bool FixedStart { get; set; }
+	public bool FixedEnd { get; set; }
+	public int PointCount { get; set; } = 32;
+	public int Iterations { get; set; } = 80;
+	public float SegmentLength { get; private set; }
+	public PhysicsWorld Physics { get; init; }
+
+	private Point[] _points;
+	public IEnumerable<Point> Points => _points;
+
+	public void Reset()
+	{
+		_points = new Point[PointCount];
+
+		var ray = new Ray( StartPosition, (EndPosition - StartPosition).Normal );
+		var distance = StartPosition.Distance( EndPosition );
+		SegmentLength = PointCount > 1
+			? distance / PointCount - 1
+			: 0f;
+
+		for ( int i = 0; i < PointCount; i++ )
+		{
+			var pos = ray.Project( SegmentLength * i );
+			_points[i] = new Point( pos, pos );
+		}
+	}
+
+	public void Simulate()
+	{
+		UpdatePoints();
+		for ( int i = 0; i < Iterations; i++ )
+		{
+			UpdateSegments();
+		}
+	}
+
+	private void UpdatePoints()
+	{
+		for ( int i = 0; i < PointCount; i++ )
+		{
+			var point = _points[i];
+			if ( FixedStart && i == 0 )
+			{
+				point.Position = StartPosition;
+				point.LastPosition = StartPosition;
+				_points[i] = point;
+				continue;
+			}
+			else if ( FixedEnd && i == PointCount - 1 )
+			{
+				point.Position = EndPosition;
+				point.LastPosition = EndPosition;
+				_points[i] = point;
+				continue;
+			}
+
+			var temp = point.Position;
+			point.Position += point.Position - point.LastPosition;
+			point.Position += Gravity * ( Time.Delta * Time.Delta );
+			point.LastPosition = temp;
+			_points[i] = point;
+		}
+	}
+
+	private void UpdateSegments()
+	{
+		for ( int i = 0; i < PointCount - 1; i++ )
+		{
+			var pointA = _points[i];
+			var pointB = _points[i + 1];
+			
+
+			var diffX = pointA.Position.x - pointB.Position.x;
+			var diffY = pointA.Position.y - pointB.Position.y;
+			var diffZ = pointA.Position.z - pointB.Position.z;
+			var distance = Vector3.DistanceBetween( pointA.Position, pointB.Position );
+			float difference = 0;
+			if ( distance > 0 )
+			{
+				difference = ( SegmentLength - distance ) / distance;
+			}
+
+			var translation = new Vector3( diffX, diffY, diffZ ) * ( 0.5f * difference );
+			pointA.Position += translation;
+			_points[i] = pointA;
+			pointB.Position -= translation;
+			_points[i + 1] = pointB;
+
+			if ( FixedStart && i == 0 )
+			{
+				_points[i] = pointA with { Position = StartPosition };
+			}
+			if ( FixedEnd && i == PointCount - 2 )
+			{
+				_points[i + 1] = pointB with { Position = EndPosition };
+			}
+		}
+	}
+}
--- code/VerletDemo.cs
+++ code/VerletDemo.cs
@@ -5,75 +5,73 @@ namespace Sandbox;
 
 public class VerletDemo : Component
 {
-	[Property, Range(0, 20)]
+	[Property, Range(0, 200)]
 	public int Iterations 
 	{
 		get => _iterations;
 		set
 		{
 			_iterations = value;
-			if ( VerletWorld is not null )
+			if ( Rope is not null )
 			{
-				VerletWorld.Iterations = value;
+				Rope.Iterations = value;
 			}
 		}
 	}
 	private int _iterations = 3;
 	[Property]
 	public Vector3 RopeStartOffset { get; set; } = Vector3.Zero;
+	public Vector3 RopeStart => WorldPosition + RopeStartOffset;
 	[Property]
 	public Vector3 RopeEndOffset { get; set; } = Vector3.Right * 50f;
-	[Property, Range( 2, 50 )]
-	public int RopePointCount { get; set; } = 10;
-	public Verlet VerletWorld { get; set; }
+	public Vector3 RopeEnd => WorldPosition + RopeEndOffset;
+	[Property, Range( 2, 200 )]
+	public int RopePointCount 
+	{
+		get => _ropePointCount;
+		set
+		{
+			var temp = _ropePointCount;
+			_ropePointCount = value;
+			if ( temp != value && Rope is not null )
+			{
+				Rope.PointCount = value;
+				Rope.Reset();
+			}
+		}
+	}
+	private int _ropePointCount = 10;
+	[Property] public bool FixedStart { get; set; } = true;
+	[Property] public bool FixedEnd { get; set; } = false;
+	public VerletRope Rope { get; set; }
 
 	protected override void OnStart()
 	{
-		VerletWorld = new()
+		Rope = new( RopeStart, RopeEnd, RopePointCount )
 		{
-			Iterations = Iterations
+			Iterations = Iterations,
+			FixedStart = FixedStart,
+			FixedEnd = FixedEnd
 		};
-		GenerateRope();
 	}
 
 	protected override void OnFixedUpdate()
 	{
-		VerletWorld.Tick();
-		foreach( var point in VerletWorld.GetPoints() )
+		Rope.StartPosition = RopeStart;
+		Rope.EndPosition = RopeEnd;
+		Rope.FixedStart = FixedStart;
+		Rope.FixedEnd = FixedEnd;
+		Rope.Simulate();
+		var points = Rope.Points.ToArray();
+		for ( int i = 0; i < points.Length; i++ )
 		{
+			var point = points[i];
 			DebugOverlay.Sphere( new Sphere( point.Position, 1f ), color: Color.Red, overlay: true );
-		}
-		foreach( var segment in VerletWorld.GetSegments() )
-		{
-			DebugOverlay.Line( segment.PointA.Position, segment.PointB.Position, color: Color.Blue, overlay: true );
-		}
-	}
-
-	private void GenerateRope()
-	{
-		var startPos = WorldPosition + RopeStartOffset;
-		var endPos = WorldPosition + RopeEndOffset;
-		var numPoints = RopePointCount;
-
-		VerletWorld.Clear();
-		var ray = new Ray( startPos, (endPos - startPos).Normal );
-		var distance = startPos.Distance( endPos );
-		var segmentLength = numPoints > 1
-			? distance / numPoints - 1
-			: 0f;
-		Verlet.Point lastPoint = null;
-		for ( int i = 0; i < numPoints; i++ )
-		{
-			var pos = ray.Project( segmentLength * i );
 			if ( i == 0 )
-			{
-				lastPoint = VerletWorld.AddPoint( pos, true );
 				continue;
-			}
 
-			var point = VerletWorld.AddPoint( pos, false );
-			VerletWorld.AddSegment( lastPoint, point, segmentLength );
-			lastPoint = point;
+			var lastPoint = points[i - 1];
+			DebugOverlay.Line( lastPoint.Position, point.Position, color: Color.Blue, overlay: true );
 		}
 	}
 
@@ -82,35 +80,53 @@ public class VerletDemo : Component
 		if ( ImGui.Begin( "Verlet Demo" ) )
 		{
 			// Debug info
-			ImGui.Text( $"Points: {VerletWorld.PointCount}, Links: {VerletWorld.SegmentCount}" );
+			ImGui.Text( $"Points: {Rope.PointCount}" );
 			ImGui.NewLine();
 
 			ImGui.Text( "Simulation Parameters" );
 			var iterations = Iterations;
 			ImGui.Text( "Iterations:" ); ImGui.SameLine();
 			ImGui.PushID( 0 );
-			ImGui.SliderInt( "Iterations", ref iterations, 0, 50 );
+			ImGui.SliderInt( "Iterations", ref iterations, 0, 200 );
 			ImGui.PopID();
 			Iterations = iterations;
 			ImGui.NewLine();
 
 			ImGui.Text( $"Rope Generation" );
 
-			// Start Pos
-			ImGui.Text( $"Start Pos:" ); ImGui.SameLine();
-			var startOffset = new Vector2( -RopeStartOffset.y, RopeStartOffset.z );
-			ImGui.PushID( 1 );
-			ImGui.SliderFloat2( "Start Offset", ref startOffset, -300f, 300f );
+			// Fixed Start
+			var fixedStart = FixedStart;
+			ImGui.PushID( "Fixed Start" );
+			ImGui.Checkbox( "Fixed Start", ref fixedStart );
 			ImGui.PopID();
-			RopeStartOffset = new Vector3( 0f, -startOffset.x, startOffset.y );
+			FixedStart = fixedStart;
+			if ( FixedStart )
+			{
+				// Start Pos
+				ImGui.Text( $"Start Pos:" ); ImGui.SameLine();
+				var startOffset = new Vector2( -RopeStartOffset.y, RopeStartOffset.z );
+				ImGui.PushID( 1 );
+				ImGui.SliderFloat2( "Start Offset", ref startOffset, -300f, 300f );
+				ImGui.PopID();
+				RopeStartOffset = new Vector3( 0f, -startOffset.x, startOffset.y );
+			}
 
-			// End Pos
-			ImGui.Text( $"End Pos:" ); ImGui.SameLine();
-			var endOffset = new Vector2( -RopeEndOffset.y, RopeEndOffset.z );
-			ImGui.PushID( 2 );
-			ImGui.SliderFloat2( "End Offset", ref endOffset, -300f, 300f );
+			// Fixed End
+			var fixedEnd = FixedEnd;
+			ImGui.PushID( "Fixed End" );
+			ImGui.Checkbox( "Fixed End", ref fixedEnd );
 			ImGui.PopID();
-			RopeEndOffset = new Vector3( 0f, -endOffset.x, endOffset.y );
+			FixedEnd = fixedEnd;
+			if ( FixedEnd )
+			{
+				// End Pos
+				ImGui.Text( $"End Pos:" ); ImGui.SameLine();
+				var endOffset = new Vector2( -RopeEndOffset.y, RopeEndOffset.z );
+				ImGui.PushID( 2 );
+				ImGui.SliderFloat2( "End Offset", ref endOffset, -300f, 300f );
+				ImGui.PopID();
+				RopeEndOffset = new Vector3( 0f, -endOffset.x, endOffset.y );
+			}
 
 			// Point Count
 			var numPoints = RopePointCount;
@@ -120,10 +136,10 @@ public class VerletDemo : Component
 			ImGui.PopID();
 			RopePointCount = numPoints;
 
-			// Generate Rope
-			if ( ImGui.Button( $"Generate Rope" ) )
+			// Reset Rope
+			if ( ImGui.Button( $"Reset Rope" ) )
 			{
-				GenerateRope();
+				Rope.Reset();
 			}
 		}
 		ImGui.End();
