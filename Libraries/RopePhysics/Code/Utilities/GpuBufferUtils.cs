d6a364d94a99ce06c6c272cb9665aa8d2a8e778f
ducc <spielerc@gmail.com>
2025-03-03 11:38:06 -0700

    Move flags check, slight cleanup

--- Libraries/RopePhysics/Code/Utilities/GpuBufferUtils.cs
+++ Libraries/RopePhysics/Code/Utilities/GpuBufferUtils.cs
@@ -2,36 +2,44 @@
 
 public static class GpuBufferUtils
 {
-	public static void EnsureCount<T>( ref GpuBuffer<T> gpuBuffer, int elementCount, GpuBuffer.UsageFlags flags = GpuBuffer.UsageFlags.Structured ) 
+	public static void EnsureCount<T>(
+		ref GpuBuffer<T> gpuBuffer,
+		int elementCount,
+		GpuBuffer.UsageFlags flags = GpuBuffer.UsageFlags.Structured
+	)
 		where T : unmanaged
 	{
-		if ( !gpuBuffer.IsValid() || gpuBuffer.Usage != flags )
+		if ( !gpuBuffer.IsValid() )
 		{
-			gpuBuffer = new( elementCount, flags );
+			gpuBuffer = new GpuBuffer<T>( elementCount, flags );
 			return;
 		}
 
-		if ( gpuBuffer.ElementCount != elementCount )
-		{
-			gpuBuffer?.Dispose();
-			gpuBuffer = new( elementCount, flags );
-		}
+		if ( gpuBuffer.ElementCount == elementCount && gpuBuffer.Usage == flags )
+			return;
+
+		gpuBuffer?.Dispose();
+		gpuBuffer = new GpuBuffer<T>( elementCount, flags );
 	}
 
-	public static void EnsureMinCount<T>( ref GpuBuffer<T> gpuBuffer, int minElementCount, GpuBuffer.UsageFlags flags = GpuBuffer.UsageFlags.Structured )
+	public static void EnsureMinCount<T>(
+		ref GpuBuffer<T> gpuBuffer,
+		int minElementCount,
+		GpuBuffer.UsageFlags flags = GpuBuffer.UsageFlags.Structured
+	)
 		where T : unmanaged
 	{
-		if ( !gpuBuffer.IsValid() || gpuBuffer.Usage != flags )
+		if ( !gpuBuffer.IsValid() )
 		{
-			gpuBuffer = new( minElementCount, flags );
+			gpuBuffer = new GpuBuffer<T>( minElementCount, flags );
 			return;
 		}
 
-		if ( gpuBuffer.ElementCount < minElementCount )
-		{
-			gpuBuffer?.Dispose();
-			gpuBuffer = new( minElementCount, flags );
-		}
+		if ( gpuBuffer.ElementCount >= minElementCount && gpuBuffer.Usage == flags )
+			return;
+
+		gpuBuffer?.Dispose();
+		gpuBuffer = new GpuBuffer<T>( minElementCount, flags );
 	}
 
 	public static void EnsureDisposedAndNull<T>( ref GpuBuffer<T> gpuBuffer )
